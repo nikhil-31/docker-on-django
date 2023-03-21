0ae0c3fd46db42cd796636bc94f4340879ad6759
nikhil-31 <nkl07ba@gmail.com>
2023-03-21 11:29:58 +0530

    Added celery frontend

--- app/hellodjango/urls.py
+++ app/hellodjango/urls.py
@@ -1,26 +1,15 @@
-"""hellodjango URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from upload.views import image_upload
+from tasks.views import home, run_task, get_status
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", image_upload, name="upload"),
+    path("upload/", image_upload, name="upload"),
+    path("tasks/<task_id>/", get_status, name="get_status"),
+    path("tasks/", run_task, name="run_task"),
+    path("", home, name="home"),
     path('admin/', admin.site.urls),
 ]
 
--- app/tasks/views.py
+++ app/tasks/views.py
@@ -1,3 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+def home(request):
+    return render(request, "tasks/home.html")
+
+
+@csrf_exempt
+def run_task(request):
+    if request.POST:
+        task_type = request.POST.get("type")
+        return JsonResponse({"task_type": task_type}, status=202)
+
+
+@csrf_exempt
+def get_status(request, task_id):
+    return JsonResponse({"task_id": task_id}, status=200)
