1665cf39087e2ae2f3a126574be8126850fbed3d
nikhil-31 <nkl07ba@gmail.com>
2023-04-04 10:56:23 +0530

    Just some random stuff

--- app/hellodjango/celery.py
+++ app/hellodjango/celery.py
@@ -4,4 +4,5 @@ from celery import Celery
 os.environ.setdefault("DEFAULT_SETTINGS_MODULE", "hellodjango.settings")
 app = Celery("hellodjango")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+# This will look inside tasks.py file
 app.autodiscover_tasks()
--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -6,6 +6,7 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 
+# Shared task allows code execution async, task is used for scheduled tasks
 @shared_task
 def create_task(task_type):
     time.sleep(int(task_type) * 10)
@@ -14,5 +15,6 @@ def create_task(task_type):
 
 @shared_task
 def sample_task():
+    logger.info("This is a scheduled task.")
     logger.info("The sample task just ran.")
     return True
