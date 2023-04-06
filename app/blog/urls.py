976d966618906ec1e4382f0261a08168536dd768
nikhil-31 <nkl07ba@gmail.com>
2023-04-06 16:58:22 +0530

    Added elastic search queries for search capability

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from blog.views import UserViewSet, CategoryViewSet, ArticleViewSet
+from blog.views import UserViewSet, CategoryViewSet, ArticleViewSet, \
+    SearchUsers, SearchArticles, SearchCategories
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
@@ -9,5 +10,8 @@ router.register(r'category', CategoryViewSet)
 router.register(r'article', ArticleViewSet)
 
 urlpatterns = [
+    path('user/<str:query>/', SearchUsers.as_view()),
+    path('category/<str:query>/', SearchCategories.as_view()),
+    path('article/<str:query>/', SearchArticles.as_view()),
     path('', include(router.urls)),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,8 +1,15 @@
+import abc
+
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from elasticsearch_dsl import Q
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.pagination import LimitOffsetPagination
 
 from blog.models import Category, Article
 from blog.serializers import CategorySerializer, ArticleSerializer, UserSerializer
+from blog.documents import UserDocument, CategoryDocument, ArticleDocument
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,3 +25,68 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+
+
+class PaginatedElasticSearchAPIView(APIView, LimitOffsetPagination):
+    serializer_class = None
+    document_class = None
+
+    @abc.abstractmethod
+    def generate_q_expression(self, query):
+        """This method should be overridden
+        and return a Q() expression."""
+
+    def get(self, request, query):
+        try:
+            q = self.generate_q_expression(query)
+            search = self.document_class.search().query(q)
+            response = search.execute()
+
+            print(f'Found {response.hits.total.value} hit(s) for query: "{query}"')
+
+            results = self.paginate_queryset(response, request, view=self)
+            serializer = self.serializer_class(results, many=True)
+            return self.get_paginated_response(serializer.data)
+        except Exception as e:
+            return HttpResponse(e, status=500)
+
+
+class SearchUsers(PaginatedElasticSearchAPIView):
+    serializer_class = UserSerializer
+    document_class = UserDocument
+
+    def generate_q_expression(self, query):
+        return Q('bool',
+                 should=[
+                     Q('match', username=query),
+                     Q('match', first_name=query),
+                     Q('match', last_name=query),
+                 ], minimum_should_match=1)
+
+
+class SearchCategories(PaginatedElasticSearchAPIView):
+    serializer_class = CategorySerializer
+    document_class = CategoryDocument
+
+    def generate_q_expression(self, query):
+        return Q(
+            'multi_match', query=query,
+            fields=[
+                'name',
+                'description',
+            ], fuzziness='auto')
+
+
+class SearchArticles(PaginatedElasticSearchAPIView):
+    serializer_class = ArticleSerializer
+    document_class = ArticleDocument
+
+    def generate_q_expression(self, query):
+        return Q(
+            'multi_match', query=query,
+            fields=[
+                'title',
+                'author',
+                'type',
+                'content'
+            ], fuzziness='auto')
